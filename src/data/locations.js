5853139edefef5be599a0e06f50816632f982ede
megamart-mgm <megagracedev@gmail.com>
2026-07-26 10:14:03 +0100

    Create locations.js

---
+++ src/data/locations.js
@@ -0,0 +1,34 @@
+const locations = [
+  {
+    name: "Lekki",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuDIHkyjGJE418u5QNmdp265r_mWnNhVMd7IE_xzFwHxecI-G3Qg0p9IFFfqU4GIRwoHNhfYCvg5Y18PXmcI5ujFQDBWyXOMLZjEHeBAZ4amjP9MUSseGS8tStviLzZ51zLzd1hZo5oxzSB8fxTrCPoi1pPvTUz9sUf_qhpgMQ7uUqO4WOPkWwJcYG-lxy09IS1ncqy4bMdlnV4DhuV9nrMiGhhI_-fpShHTBcpH8YbeAUndTLFIS5gG7V_cbyiOllMdhLEO_mopJig",
+  },
+  {
+    name: "Ikoyi",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuAS3OXWZfaEqJ7g3msn1U16QNivYMw5FvQvFwd1vDdqvAXa3Vy3Ze0MrPRZFHXRfucmuKrK7QqHObnusT0IXqimF62wqD_VHfmQZ_f25-KdDrTmfYzAOILjeJPL3cFT441CILlUZfwvZvVFAcM6UMGGKaZOy2iKwAKtyS7mhubjy_KldwpJeTJfH4nl6LKS0CXUWFGIXlTB_5p6mQWFN9pHbbaZlAyf1wB0ElBTWPLyy3RLpuoQglattuwraNLMdbHQy6D9YvxS47M",
+  },
+  {
+    name: "Victoria Island",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuC9MULCOR1LwLKdMA0ZzWmXhv5DS94M6gL_f5qHtB7q4gZ1QObZsWaGoPWb8-nUqll1BDbCG-l-wMRdCpayn2-X8EnuR2DLZTZ0xj1uAJburCx-nPg1nAUXx2RwhV9w_eo2nAYxBB8CgGiGFY8UcnnSmtrT6r-uimzn5yo-9Bq7HU5bL3nHX2jrKz3PzaL0iitEG51CBGK7YfBWEsQkXk-C6_47yQhXUa99B8WfkvOq1CfjQ9qo11JgMpHHRsU_x32T34-U6-Tbt2Y",
+  },
+  {
+    name: "Ajah",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuB4J_yxYz2MOk-xctiOrOjiJZQziE9sG4Tzk2LHrG_a_LQ9aI1jwYiYCtSd7-rnzxRpVPewNffEKz4aiaTcmBY8vlcAHx5BuPaz4kxnfifR2Td7c7xxlzpMEIqrT1TnseKM0In6ehJZLNuUxRZKJurC7zadDBg1KamO9D3yAhW24n2LeZorEZJwkH3OKilmKj7ifx-PsfvCLb_GyFNaoYkyd3D28Kr9p6_199L69OMfC1mYFCnGRfG03SVaWbmfQc9AFuMvnFkJTJs",
+  },
+  {
+    name: "Ibeju Lekki",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuDI6Nj4pLJKJ7d2MheEmrNG16h-BMh0E_cYMsGKymS5M0A_VksPR9anlTZ4EMLjEQhMz7ow5YcelfH5MeDysjs0fGdEPZh6V8RSXjNtLwwTqBJ4OxExcasQp44ZHl94foC71ev91Fm1fB45hR8ZVfprP6rjDLiyywkCKWTWYQ2ffNYvNAzdH7m2KH5ba2aen5EqeRGpa3E9dPf9EIaGvKNK6HXERkM9_PhG43migVtJs81QmzX6R2QdxJLsTlwr1FZ2Z0HYrXqK2AI",
+  },
+  {
+    name: "Abuja",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuAnoRKyeVHgPCdXTet3UXAmy7C-qedWxGpnHP3QrDcx0GHN_cXOSPr3-ywuqoAquQENmuQp0kWwFmRH8Pb7mfnrCqsV3R1ZO7OqdseVJNlIVtj0uAWIj1lyU3DDTvaR7lKozwEJu9kghKjudq3eZuBQ0osciejv55ZjNr7b2Zxn1xrQT_89mLFhbRWKg69mbBr9lhbO7JtGgRFMli8EpdvhR9Z-vQ6FFx6k-MNdxU-ThdGvOqqMr0nMDTYJzMY-VzUST3zlz9-D9xA",
+  },
+];
+
+export default locations;
